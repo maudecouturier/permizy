60f3a6bd7d02a1481d82335b6cf290b0a16e1f91
Maude Couturier <maude.couturier2@gmail.com>
2020-02-25 14:43:09 +0100

    CRUD

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,5 @@
 class BookingsController < ApplicationController
   def index
-    # if current_user.teacher
-    #   @bookings = current_user.teacher_bookings
-    # else
-    #   @bookings = current_user.student_bookings
-    # end
     @bookings = policy_scope(Booking)
     authorize @bookings
   end
@@ -13,4 +8,45 @@ class BookingsController < ApplicationController
     @booking = Booking.find(params[:id])
     authorize @booking
   end
+
+  def new
+    @booking = Booking.new
+    authorize @booking
+
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @student = current_user
+    @booking.student = @student
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :new
+    end
+    authorize @booking
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+    authorize @booking
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    if @booking.update(booking_params)
+      redirect_to bookings_path
+    else
+      render :new
+    end
+    authorize @booking
+  end
+
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:teacher_id, :slot)
+  end
+
 end
