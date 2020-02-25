eb8ae946f99eeaa5e573cc3d7a56cd940df8150d
Maude Couturier <maude.couturier2@gmail.com>
2020-02-25 16:11:50 +0100

    model validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,16 @@
 class Booking < ApplicationRecord
   belongs_to :student, class_name: 'User', foreign_key: 'student_id'
   belongs_to :teacher, class_name: 'User', foreign_key: 'teacher_id'
+  validates :slot, :teacher, :user, presence: true
+  validate :slot_validation
+
+
+  def slot_validation
+    return if slot.blank?
+
+    if slot < DateTime.now() + 86_400
+      errors.add(:slot, "Chosen time cannot be before tomorrow")
+    end
+  end
+
 end
