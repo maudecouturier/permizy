ab88be2e6b28a4ba7c4b30fe224495a0afdde748
Alex Huyn <huyn.alex@gmail.com>
2020-02-26 17:41:02 +0100

    migration ok

---
+++ db/migrate/20200226163355_create_slots.rb
@@ -0,0 +1,11 @@
+class CreateSlots < ActiveRecord::Migration[6.0]
+  def change
+    create_table :slots do |t|
+      t.datetime :start
+      t.datetime :end
+      t.references :booking, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
