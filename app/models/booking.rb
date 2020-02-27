e3e21c9f0304fd5efba6939581034db06112242c
Alex Huyn <huyn.alex@gmail.com>
2020-02-27 10:31:01 +0100

    model update

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :student, class_name: 'User', foreign_key: 'student_id'
-
+  has_one :slot
   validates  :student, presence: true
 
   geocoded_by :address
