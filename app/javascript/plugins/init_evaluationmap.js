7ce8368ca6f2344ab26be6166192ce5365e04ca5
Alex Huyn <huyn.alex@gmail.com>
2020-03-03 19:41:58 +0100

    additional security but doesnt work

--- app/javascript/plugins/init_evaluationmap.js
+++ app/javascript/plugins/init_evaluationmap.js
@@ -34,44 +34,47 @@ const fitMapToMarkers = (map, markers) => {
 };
 
 const initEvaluationmap = () => {
-  mapboxgl.accessToken = mapElementEval.dataset.mapboxApiKey;
-  const evalMap = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10',
-    center: [-122.486052, 37.830348],
-    zoom: 15
-  });
-  addMarkersToMap(evalMap, markers);
-  fitMapToMarkers(evalMap, markers);
-  // displayFlashcard
-
-  evalMap.on('load', function() {
-        evalMap.addSource('route', {
-            'type': 'geojson',
-            'data': {
-                'type': 'Feature',
-                'properties': {},
-                'geometry': {
-                    'type': 'LineString',
-                    'coordinates': coordinates
-                }
-            }
-        });
-        evalMap.addLayer({
-            'id': 'route',
-            'type': 'line',
-            'source': 'route',
-            'layout': {
-                'line-join': 'round',
-                'line-cap': 'round'
-            },
-            'paint': {
-                'line-color': '#888',
-                'line-width': 8
-            }
-        });
+  if (mapElementEval) {
+    mapboxgl.accessToken = mapElementEval.dataset.mapboxApiKey;
+    const evalMap = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10',
+      center: [-122.486052, 37.830348],
+      zoom: 15
     });
-  };
+    addMarkersToMap(evalMap, markers);
+    fitMapToMarkers(evalMap, markers);
+    // displayFlashcard
+
+    evalMap.on('load', function() {
+          evalMap.addSource('route', {
+              'type': 'geojson',
+              'data': {
+                  'type': 'Feature',
+                  'properties': {},
+                  'geometry': {
+                      'type': 'LineString',
+                      'coordinates': coordinates
+                  }
+              }
+          });
+          evalMap.addLayer({
+              'id': 'route',
+              'type': 'line',
+              'source': 'route',
+              'layout': {
+                  'line-join': 'round',
+                  'line-cap': 'round'
+              },
+              'paint': {
+                  'line-color': '#888',
+                  'line-width': 8
+              }
+          });
+      });
+
+  }
+};
 
 
 
